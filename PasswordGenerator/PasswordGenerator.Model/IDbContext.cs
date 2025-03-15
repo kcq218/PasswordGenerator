cdfc6b21d8be123a56e9f0043b411bd67bed92d3
kcq218 <khangquach218@gmail.com>
2025-03-15 12:33:04 -0700

    DI finished

---
+++ PasswordGenerator/PasswordGenerator.Model/IDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PasswordGenerator.Model;
+
+public interface IDbContext : IDisposable
+{
+    DbContext Context { get; }
+    DbSet<PasswordGenerator> PasswordGenerators { get; set; }
+
+    DbSet<T> Set<T>() where T : class;
+    public int SaveChanges();
+}
