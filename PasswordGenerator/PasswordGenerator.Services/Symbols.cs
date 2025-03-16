503c5512dcb6a2e58bd38f9ba26009a43f8d2541
kcq218 <khangquach218@gmail.com>
2025-03-15 20:03:56 -0700

    finished program

--- PasswordGenerator/PasswordGenerator.Services/Symbols.cs
+++ PasswordGenerator/PasswordGenerator.Services/Symbols.cs
@@ -1,10 +1,12 @@
-﻿
-namespace PasswordGenerator.Services
+﻿namespace PasswordGenerator.Services
 {
     public class Symbols : AbstractOption
     {
         public override int Length { get; set; }
         public override bool Included { get; set; }
-        public override string Characters { get; set; }
+        public override string Characters()
+        {
+            return "!@#$%^&*()_+=-`~<>{}[]";
+        }
     }
 }
